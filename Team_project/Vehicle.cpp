6f483bd436855d87e2fe6e50d5e13c90fbef28a4
TanhTongTai <hoangphuctuananh03@gmail.com>
2025-05-12 21:28:42 +0700

    tanhh

---
+++ Team_project/Vehicle.cpp
@@ -0,0 +1,87 @@
+#include "Vehicle.h"
+
+Vehicle::Vehicle()
+{
+	this->price = 0;
+	this->brand = "";
+	this->model = "";
+}
+
+Vehicle::Vehicle(string brand, string model, double price)
+{
+	this->brand = brand;
+	this->model = model;
+	this->price = price;
+}
+
+string Vehicle::getBrand()
+{
+	return this->brand;
+}
+
+string Vehicle::getModel()
+{
+	return this->model;
+}
+
+double Vehicle::getPrice()
+{
+	return this->price;
+}
+
+void Vehicle::setBrand(string b)
+{
+	this->brand = b;
+}
+
+void Vehicle::setModel(string m)
+{
+	this->model = m;
+}
+
+void Vehicle::setPrice(double g)
+{
+	this->price = g;
+}
+
+void Vehicle::displayInf()
+{
+	cout <<"Vehicle's Information:" << brand << " " << model << " " << price << endl;
+}
+
+bool Vehicle::operator<(const Vehicle& p) const
+{
+	if (this->price < p.price) {
+		return true;
+	}
+	return false;
+}
+
+bool Vehicle::operator>(const Vehicle& q) const
+{
+	return this->price > q.price;
+}
+
+bool Vehicle::operator=(const Vehicle& r) const
+{
+	if (this->price == r.price) {
+		return true;
+	}
+	return false;
+}
+
+ostream& operator<<(ostream& os, const Vehicle& v)
+{
+	os << "The brand: " << v.brand << endl;
+	os << "The model: " << v.model << endl;
+	os << "The price: " << v.price << endl;
+	return os;
+}
+
+istream& operator>>(istream& is, Vehicle& t)
+{
+	is >> t.brand;
+	is >> t.model;
+	is >> t.price;
+	return is;
+}
---
+++ Team_project/Vehicle.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+class Vehicle
+{
+private:
+	string brand;
+	double price;
+	string model;
+public:
+	Vehicle();
+	Vehicle(string, string, double);
+	string getBrand();
+	string getModel();
+	double getPrice();
+	void setBrand(string);
+	void setModel(string);
+	void setPrice(double);
+	virtual double fuelConsumption() = 0;
+	virtual void displayInf();
+	bool operator < (const Vehicle&) const;
+	bool operator > (const Vehicle&) const;
+	bool operator = (const Vehicle&) const;
+	friend ostream& operator << (ostream&, const Vehicle&);
+	friend istream& operator >> (istream&, Vehicle&);
+};
+
